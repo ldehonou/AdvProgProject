233153e773c308d38c1081af51b6bf3736aa86f3
Théo Cohen <theo.cohen@efrei.net>
2023-02-28 10:29:41 +0100

    Update : loginServlet

--- src/main/java/com/st2apr/jpaintro_08022023/LoginServlet.java
+++ src/main/java/com/st2apr/jpaintro_08022023/LoginServlet.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.Collection;
 
 import jakarta.ejb.EJB;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -19,23 +20,42 @@ public class LoginServlet extends HttpServlet {
     public void init() {}
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Collection<TutorEntity> tutors = tutorBean.getAllTutors();
-
-        for (TutorEntity tutor: tutors) {
-            System.out.println("NAME : "+ tutor.getEmail());
-        }
-    }
-
-    @Override
     public void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String identifiant = request.getParameter("identifiant");
         String password = request.getParameter("password");
-        System.out.println("ID" + identifiant);
-        System.out.println("Pass " + password);
 
+<<<<<<< Updated upstream
         request.setAttribute("message", "Hello " + identifiant + " !");
         request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+=======
+        System.out.println(identifiant);
+        System.out.println(password);
+
+        TutorEntity tutor = null;
+        try {
+            // Query the database to find the user with the given username and password
+            tutor = tutorBean.getTutors(identifiant, password);
+
+            System.out.println(tutor.getEmail());
+            // If the user is found, set the user object in the session and redirect to the home page
+
+        } catch (Exception e) {
+            // If the user is not found, display an error message and forward to the login page
+            System.out.println("Invalid username or password");
+            request.setAttribute("error", "Invalid username or password");
+        }
+
+        if (tutor != null) {
+            System.out.println("Try Redirect");
+            HttpSession session = request.getSession(true);
+            session.setAttribute("identifiant", tutor.getEmail());
+            response.sendRedirect(request.getContextPath() + "/home");
+
+        } else {
+            System.out.println("Fail Redirect");
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+>>>>>>> Stashed changes
     }
 
     public void destroy() {
